5cc6533e4b833b5a6d062a95d3197cd485951c8b
Meghla15 <meherunnesa1505@gmail.com>
2024-04-15 23:00:10 +0600

    updateProfile created

--- src/DetailsPage/CardDetails.jsx
+++ src/DetailsPage/CardDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import useCartData from '../Hooks/UseCardData';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 const CardDetails = () => {
@@ -20,33 +20,33 @@ const CardDetails = () => {
         }
     },[cardData,id]);
 
-    const {segment_name,estate_title,facilities } = singleData || {}
+    const {segment_name,estate_title,description, price, area,location,image_url ,status , facilities } = singleData || {}
    
     return (
         <div className="hero min-h-screen container mx-auto rounded-2xl bg-base-200 mb-40">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="https://assets.lbmjournal.com/uploads/2019/11/new-single-family-home.jpg" className="w-[50%] rounded-lg shadow-2xl" />
+          <img src={image_url}className="w-[50%] rounded-lg shadow-2xl" />
           <div className=''>
-          <p className='border-[1px] bg-red-700 text-white rounded-3xl w-1/3 text-center p-2 mb-2'>Status</p>
-            <h1 className="text-2xl font-neon font-semibold mb-2">Estate Name : {estate_title}</h1>
-            <h1 className="text-2xl font-neon font-bold">Estate Title :<span className='font-extrabold font-neon '>Spacious Family Home in Suburbia</span></h1>
+          <p className='border-[1px] bg-red-700 text-white font-bold text-xl rounded-3xl w-1/3 text-center p-2 mb-2'>{status}</p>
+            <h1 className="text-2xl font-neon font-semibold mb-2">Estate Name : <span className='font-extrabold font-neon '>{estate_title}</span></h1>
+            <h1 className="text-2xl font-neon font-bold">Estate Title :<span className='font-extrabold font-neon '>{segment_name}</span></h1>
           
-            <p> <span className="py-6 text-xl font-semibold">Description: </span> This beautiful single-family home boasts 4 bedrooms, 2 bathrooms, and a large backyard, making it perfect for families seeking comfort and space. Nestled in the serene suburbs, this property offers a peaceful retreat from the hustle and bustle of city life. With ample natural light and modern amenities, including a spacious living room and fully equipped kitchen, this home is ideal for both relaxation and entertaining guests. Enjoy outdoor gatherings or simply unwind in the privacy of your backyard oasis. Conveniently located near schools, parks, and shopping centers, this home offers the perfect blend of tranquility and convenience.</p>
+            <p> <span className="py-6 text-xl font-semibold">Description: </span> {description}</p>
 
 
-            <div className="flex lg:flex-row flex-col gap-4">
-        <div className="flex">
+            <div className="flex lg:flex-row flex-col gap-4 mt-3">
+        <div className="flex ">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-orange-600">
        <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
      </svg>
-         <p className="font-semibold text-neon"></p>
+         <p className="font-semibold text-neon"><span  className="font-semibold">Location: </span>{location}</p>
         </div>
         <div className="flex">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-orange-500">
        <path fill-rule="evenodd" d="M8.161 2.58a1.875 1.875 0 0 1 1.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0 1 21.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 0 1-1.676 0l-4.994-2.497a.375.375 0 0 0-.336 0l-3.868 1.935A1.875 1.875 0 0 1 2.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437ZM9 6a.75.75 0 0 1 .75.75V15a.75.75 0 0 1-1.5 0V6.75A.75.75 0 0 1 9 6Zm6.75 3a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Z" clip-rule="evenodd" />
      </svg>
      
-         <p><span  className="font-semibold" >Area:</span></p>
+         <p><span  className="font-semibold" >Area: </span> {area}</p>
      
         </div>
         </div>
@@ -58,9 +58,24 @@ const CardDetails = () => {
                     <li className='opacity-70' key={index}> {facilities}</li>
                      ))}
                    </ul>
-                   <p className='font-semibold font-neon text-2xl mt-3'>Price : <span className='font-neon font-extrabold '> 2500000$</span></p>
+                   <p className='font-semibold font-neon text-2xl mt-3'>Price : <span className='font-neon font-extrabold '> {price}$</span></p>
+                   {/* <button className='btn bg-green-500 w-32 text-white'></button> */}
+
+                  <Link to="/">
+                  <a href="#_" className=" mt-2 relative inline-block text-lg group">
+<span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+<span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-green-500"></span>
+<span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+<span class="relative">Back to Home</span>
+</span>
+<span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+</a>
+                  </Link>
           </div>
+          
         </div>
+
+      
       </div>
 
     );
--- src/Pages/UpdateProfile.jsx
+++ src/Pages/UpdateProfile.jsx
@@ -1,66 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const UpdateProfile = () => {
+    const [showPassword, setShowPassword] = useState(false);
   return (
     <div>
       <div className="w-2/4 mx-auto mt-12">
-        <h1 className="animate__animated animate__bounce text-center font-neon text-3xl font-bold mb-2">
+        <h1 className="animate__animated animate__bounce text-center font-neon lg:text-3xl text-xl font-bold mb-2">
           Update Your Profile
         </h1>
         <form>
-          <label className="input input-bordered flex items-center gap-2 mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-            </svg>
-            <input type="text" className="grow" placeholder="Email" />
-          </label>
-          <label className="input input-bordered flex items-center gap-2 mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input type="text" className="grow" placeholder="Username" />
-          </label>
-          <label className="input input-bordered flex items-center gap-2 mb-2">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input type="" className="" value="password" />
-          </label>
-          <label className="input input-bordered flex items-center gap-2 mb-2">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input type="setPassword" className="" value="setPassword" />
-          </label>
+          <input  type="text" placeholder="Your Name" className="input mb-2 input-bordered input-primary w-full mx-auto" />
+          <input required type="email" placeholder="Email" className="input mb-2 input-bordered input-primary w-full mx-auto" />
+        <div className="relative">
+        
+          <input required type={showPassword ? "text" : "password"}placeholder="Password" className="input mb-2 input-bordered input-primary w-full mx-auto" /> 
+          <span className="absolute right-1 bottom-[20px]" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaEye></FaEye>  : <FaEyeSlash></FaEyeSlash>}</span>
+        </div>
+         
+        <div className="relative">
+        
+          <input required type={showPassword ? "text" : "password"}placeholder="Set Password" className="input mb-2 input-bordered input-primary w-full mx-auto" /> 
+          <span className="absolute right-1 bottom-[20px]" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaEye></FaEye>  : <FaEyeSlash></FaEyeSlash>}</span>
+        </div>
+         
           <button className="btn btn-primary mx-auto w-full font-bold text-xl mb-44">Submit</button>
         </form>
       </div>
